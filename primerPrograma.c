c949d3dcb5c8b9adcdf2359142f4ded7ec7bb5ee
Fernando <fh1273509@gmail.com>
2022-12-01 21:08:24 -0300

    primer hola mundo en c

---
+++ primerPrograma.c
@@ -0,0 +1,14 @@
+//primer programa
+ 
+//cargando las librerias
+
+#include <stdio.h>
+
+//int main es la funcion principal
+int main(){
+		//printf es la funcion que nos permite que aparezca el mensaje por pantalla
+	printf("Hola Mundo.\n"); //.\n nos permite dar un salto de linea
+	
+	system ("pause");
+	return 0;
+}
