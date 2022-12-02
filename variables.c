a175575a0805b8e6071ee047f16dc1eb42ec84c3
Fernando <fh1273509@gmail.com>
2022-12-01 21:33:40 -0300

    variables

---
+++ variables.c
@@ -0,0 +1,32 @@
+//variables
+ 
+//cargando las librerias
+
+#include <stdio.h>
+
+
+
+
+int main(){
+
+int x;//numeros enteros
+float y;//numeros con decimales 32bits
+double z;//numeros decimales 64bits
+char s;//caracteres
+
+
+ x = 5;
+ y = 2.35;
+ z = 25.1;
+ s = 'a'; 
+	
+	
+		printf("El resultado de x es : %i.\n", x);		
+		printf("El resultado de y es : %i.\n", y);	
+		printf("El resultado de z es : %i.\n", z);	
+		printf("El resultado de s es : %c.\n", s);	
+	
+	
+	system ("pause");
+	return 0;
+}
