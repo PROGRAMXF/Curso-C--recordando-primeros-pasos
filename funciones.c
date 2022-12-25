57aa6348767f216824e9e04a97322417b72ab5da
Fernando <fh1273509@gmail.com>
2022-12-25 19:31:04 -0300

    funciones

---
+++ funciones.c
@@ -0,0 +1,37 @@
+//calcular el numero mayor entre dos numeros con una funcion
+
+
+#include <stdio.h>
+
+
+int maximo();
+
+int main(){
+	
+	int x, y, max;
+	
+	x = 3;
+	y = 10;
+	
+	max = maximo(x, y);	
+	
+	printf("El valor maximo es %i .\n\n",max);
+	
+	system("pause");
+	return 0;	
+	
+}
+
+int maximo (int a, int b){ //pase de parametros: x = a  e  y = b
+
+	int aux;
+	
+	if(a > b){
+		aux = a;
+	}
+	else{
+		aux = b;
+	}	
+
+	return aux;
+}
